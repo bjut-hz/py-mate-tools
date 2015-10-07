079ddeaa3d43a0c07e1fa9bf34a7f8baa04f2dfd
何震震 <936432896@qq.com>
2015-10-07 19:36:36 +0800

    finish data format func

--- test/DataFormat.py
+++ test/DataFormat.py
@@ -6,21 +6,41 @@ class DataFormat:
         pass
 
     def dataFormat( self, _sentence ):
-
+        """
+        format the input data and save it in the tmp file
+        :param _sentence:
+        :return:
+        """
         if isinstance( _sentence, list ):
+            result = []
             for sentence in _sentence:
-                words = nltk.word_tokenize( sentence )
-                for word in words:
-                    pass
-                pass
+                result.extend( self.__dataFormat( sentence) )
+                result.append( '\n' )
+            self.__save( result)
+            return result
         elif isinstance( _sentence, str ):
-            pass
+            result = self.__dataFormat( _sentence )
+            self.__save( result )
+            return result
         else:
             return  None
 
+    def __dataFormat( self, sentence ):
+        result = []
+        words = nltk.word_tokenize( sentence )
+        for word in words:
+            word = ' ' + word + '\n'
+            result.append( word )
+        return result
+
+    def __save( self, data ):
+        f = file( 'tmp', 'w+' )
+        f.writelines( data )
+        f.close()
+
 
 if __name__ == '__main__':
-    f = file( 'hell.txt', 'w+')
-    data = [' hello world\n', ' hello python\n']
-    f.writelines( data )
-    f.close()
+    testdata = ['I saw a dog chasing a cat.', 'I love you.']
+    data_format = DataFormat()
+    data_format.dataFormat( testdata )
+    print( "test" )

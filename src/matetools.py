28a4d80b7077d3c674be45321cfa81addea48bac
何震震 <936432896@qq.com>
2015-10-30 16:08:48 +0800

    add the mate tools python interface file

---
+++ src/matetools.py
@@ -0,0 +1,13 @@
+__author__ = 'hz'
+
+class MateTools():
+    JARS = r'srl.jar;lib\anna-3.3.jar;lib\liblinear-1.51-with-deps.jar;lib\opennlp-tools-1.5.2-incubating.jar;lib\opennlp-maxent-3.0.2-incubating.jar;lib\seg.jar'
+    LEMMA_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.lemmatizer.model'
+    TAGGER_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.postagger.model'
+    PARSER_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.parser.model'
+    SRL_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.srl-4.1.srl.model'
+    
+    def __init__(self):
+        pass
+
+

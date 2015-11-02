0dfc1eb9cdd6205470e3e4ee3371b305669d2075
何震震 <936432896@qq.com>
2015-11-02 10:39:33 +0800

    python interface

--- src/matetools.py
+++ src/matetools.py
@@ -6,8 +6,15 @@ class MateTools():
     TAGGER_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.postagger.model'
     PARSER_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.parser.model'
     SRL_MODEL = r'models\CoNLL2009-ST-English-ALL.anna-3.3.srl-4.1.srl.model'
-    
+    OUTPUT_FORMAT = r"conll2009"
+
+
     def __init__(self):
         pass
 
+    def SRL( self, sentence, cwd = r"./", file_name = r"result" ):
+        cmd = []
+        self._execute( cmd )
 
+    def _execute(self, cmd ):
+        pass
